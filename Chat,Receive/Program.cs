c214c1041333587806b6d60fa39f2f8f58e0bb06
JoseGarciaS <72817489+JoseGarciaS@users.noreply.github.com>
2023-03-08 20:06:48 -0600

    Add project files.

---
+++ Chat,Receive/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "messages-queue", false, false, false, null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"{message}");
+};
+channel.BasicConsume(queue: "messages-queue", autoAck: true, consumer: consumer);
+
+Console.ReadLine();
---
+++ Chat.Send/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "messages-queue", false, false, false, null);
+
+const string message = "Hola";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty,  "messages",
+                     basicProperties: null,
+                     body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
